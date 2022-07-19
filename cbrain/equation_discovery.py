d95d6d6ab024dbfc1bcc2a39b28595730093d2ca
Tom Beucler <tom.beucler@gmail.com>
2022-07-18 17:30:10 -0700

    Generalized the equation discovery library a bit to relax subsampling

--- cbrain/equation_discovery.py
+++ cbrain/equation_discovery.py
@@ -61,7 +61,7 @@ def p_der_spline(var,pressure_data,pressure_eval,order,kind='cubic'):
     
     return output
 
-def subsampler(ind,x,y,xRH,xB,xLHFns,xt,yt,xRHt,xBt,xLHFnst,hyam,hybm,
+def subsampler(ind,x,y,xRH,xB,xLHFns,hyam,hybm,
                variables=['p','q','dq_dp','dq_dp_FD','d2q_dp2',
                          'd2q_dp2_FD','Q_above','Q_below','T',
                          'dT_dp','d2T_dp2','dT_dp_FD','d2T_dp2_FD',
@@ -70,32 +70,45 @@ def subsampler(ind,x,y,xRH,xB,xLHFns,xt,yt,xRHt,xBt,xLHFnst,hyam,hybm,
                          'RH_above','RH_below','B','dB_dp',
                           'd2B_dp2','dB_dp_FD','d2B_dp2_FD',
                           'B_above','B_below','ps','S0','SHF',
-                          'LHF','LHFns','dq_dt','dT_dt']):
+                          'LHF','LHFns','dq_dt','dT_dt'],
+               xt=None,yt=None,xRHt=None,xBt=None,xLHFnst=None):
     """
     Subsamples and formats data from various sources into a 
     dictionary of inputs and outputs used for equation discovery
-    ind = Subsampling indices
+    ind = Subsampling indices. None to select the full array.
     x = Input vector containing raw variables
     xRH = Input vector containing relative humidity
     xB = Input vector containing plume buoyancy
     xLHFns = Input vector containing rescaled latent heat fluxes
-    xt, yt, xRHt, xBt, xLHFnst = Same but for the test set
     hyam,hybm = Used to calculate the hybrid sigma vertical coordinate
+    xt, yt, xRHt, xBt, xLHFnst = (Optional) Same but for the test set
     variables = Variables to include in the formatted dataset
     """
     
+    if xt is not None: 
+        climates = xt.keys();
+        xt_sub = {}; yt_sub = {}; 
+        xRHt_sub = {}; xBt_sub = {}; xLHFnst_sub = {};
     # Sub-sample the training set
-    x_sub = x[ind,:]; y_sub = y[ind,:]; 
-    xRH_sub = xRH[ind,:]; xB_sub = xB[ind,:]; xLHFns_sub = xLHFns[ind];
-    # Sub-sample the test sets
-    xt_sub = {}; yt_sub = {}; xRHt_sub = {}; xBt_sub = {}; xLHFnst_sub = {};
-    climates = xt.keys()
-    for iclim,clim in enumerate(climates):
-        xt_sub[clim] = xt[clim][ind,:]
-        yt_sub[clim] = yt[clim][ind,:]
-        xRHt_sub[clim] = xRHt[clim][ind,:]
-        xBt_sub[clim] = xBt[clim][ind,:]
-        xLHFnst_sub[clim] = xLHFnst[clim][ind]
+    if ind is not None:
+        x_sub = x[ind,:]; y_sub = y[ind,:]; 
+        xRH_sub = xRH[ind,:]; xB_sub = xB[ind,:]; xLHFns_sub = xLHFns[ind];
+        # Sub-sample the test sets
+        if xt is not None:
+            for iclim,clim in enumerate(climates):
+                xt_sub[clim] = xt[clim][ind,:]
+                yt_sub[clim] = yt[clim][ind,:]
+                xRHt_sub[clim] = xRHt[clim][ind,:]
+                xBt_sub[clim] = xBt[clim][ind,:]
+                xLHFnst_sub[clim] = xLHFnst[clim][ind]
+    else: 
+        # Do not sub-sample the training nor the test set
+        x_sub = x; y_sub = y; xRH_sub = xRH; xB_sub = xB; xLHFns_sub = xLHFns;
+        if xt is not None:
+            for iclim,clim in enumerate(climates):
+                xt_sub[clim] = xt[clim]; yt_sub[clim] = yt[clim];
+                xRHt_sub[clim] = xRHt[clim]; xBt_sub[clim] = xBt[clim];
+                xLHFnst_sub[clim] = xLHFnst[clim]
     # Pressure
     P0 = 1e5 # Standard value in CBRAIN repository
     PS = x_sub[:,60]; pm = P0*hyam+np.outer(PS,hybm)
@@ -140,54 +153,55 @@ def subsampler(ind,x,y,xRH,xB,xLHFns,xt,yt,xRHt,xBt,xLHFnst,hyam,hybm,
     y_train['dT_dt'] = y_sub[:,30:60].values
     # Test structure
     x_test = {}; y_test = {}
-    for iclim,clim in enumerate(climates):
-        print(clim)
-        x_test[clim] = {}; y_test[clim] = {};
-        if 'p' in variables: x_test[clim]['p'] = P0*hyam+np.outer(xt_sub[clim][:,60].values,hybm)
-        if 'q' in variables: x_test[clim]['q'] = xt_sub[clim][:,:30].values
-        if 'dq_dp' in variables: x_test[clim]['dq_dp'] = p_der_spline(x_test[clim]['q'],x_test[clim]['p'],x_test[clim]['p'],1)
-        if 'dq_dp_FD' in variables: x_test[clim]['dq_dp_FD'] = p_derivative(x_test[clim]['q'],x_test[clim]['p'])
-        if 'd2q_dp2' in variables: x_test[clim]['d2q_dp2'] = p_der_spline(x_test[clim]['q'],x_test[clim]['p'],x_test[clim]['p'],2)
-        if 'd2q_dp2_FD' in variables: x_test[clim]['d2q_dp2_FD'] = p_derivative(x_test[clim]['dq_dp_FD'],x_test[clim]['p'])
-        if 'Q_above' in variables: x_test[clim]['Q_above'] = p_integral(x_test[clim]['q'],x_test[clim]['p'],'above')
-        if 'Q_below' in variables: x_test[clim]['Q_below'] = p_integral(x_test[clim]['q'],x_test[clim]['p'],'below')
-        if 'T' in variables: x_test[clim]['T'] = xt_sub[clim][:,30:60].values
-        if 'dT_dp' in variables: x_test[clim]['dT_dp'] = p_der_spline(x_test[clim]['T'],x_test[clim]['p'],x_test[clim]['p'],1)
-        if 'dT_dp_FD' in variables: x_test[clim]['dT_dp_FD'] = p_derivative(x_test[clim]['T'],x_test[clim]['p'])
-        if 'd2T_dp2' in variables: x_test[clim]['d2T_dp2'] = p_der_spline(x_test[clim]['T'],x_test[clim]['p'],x_test[clim]['p'],2)
-        if 'd2T_dp2_FD' in variables: x_test[clim]['d2T_dp2_FD'] = p_derivative(x_test[clim]['dT_dp_FD'],x_test[clim]['p'])
-        if 'T_above' in variables: x_test[clim]['T_above'] = p_integral(x_test[clim]['T'],x_test[clim]['p'],'above')
-        if 'T_below' in variables: x_test[clim]['T_below'] = p_integral(x_test[clim]['T'],x_test[clim]['p'],'below')
-        if 'RH' in variables: x_test[clim]['RH'] = xRHt_sub[clim].values
-        if 'dRH_dp' in variables: x_test[clim]['dRH_dp'] = p_der_spline(x_test[clim]['RH'],x_test[clim]['p'],x_test[clim]['p'],1)
-        if 'dRH_dp_FD' in variables: x_test[clim]['dRH_dp_FD'] = p_derivative(x_test[clim]['RH'],x_test[clim]['p'])
-        if 'd2RH_dp2' in variables: x_test[clim]['d2RH_dp2'] = p_der_spline(x_test[clim]['RH'],x_test[clim]['p'],x_test[clim]['p'],2)
-        if 'd2RH_dp2_FD' in variables: x_test[clim]['d2RH_dp2_FD'] = p_derivative(x_test[clim]['dRH_dp_FD'],x_test[clim]['p'])
-        if 'RH_above' in variables: x_test[clim]['RH_above'] = p_integral(x_test[clim]['RH'],x_test[clim]['p'],'above')
-        if 'RH_below' in variables: x_test[clim]['RH_below'] = p_integral(x_test[clim]['RH'],x_test[clim]['p'],'below')
-        if 'B' in variables: x_test[clim]['B'] = xBt_sub[clim].values
-        if 'dB_dp' in variables: x_test[clim]['dB_dp'] = p_der_spline(x_test[clim]['B'],x_test[clim]['p'],x_test[clim]['p'],1)
-        if 'dB_dp_FD' in variables: x_test[clim]['dB_dp_FD'] = p_derivative(x_test[clim]['B'],x_test[clim]['p'])
-        if 'd2B_dp2' in variables: x_test[clim]['d2B_dp2'] = p_der_spline(x_test[clim]['B'],x_test[clim]['p'],x_test[clim]['p'],2)
-        if 'd2B_dp2_FD' in variables: x_test[clim]['d2B_dp2_FD'] = p_derivative(x_test[clim]['dB_dp_FD'],x_test[clim]['p'])
-        if 'B_above' in variables: x_test[clim]['B_above'] = p_integral(x_test[clim]['B'],x_test[clim]['p'],'above')
-        if 'B_below' in variables: x_test[clim]['B_below'] = p_integral(x_test[clim]['B'],x_test[clim]['p'],'below')
-        if 'ps' in variables: x_test[clim]['ps'] = xt_sub[clim][:,60].values
-        if 'S0' in variables: x_test[clim]['S0'] = xt_sub[clim][:,61].values
-        if 'SHF' in variables: x_test[clim]['SHF'] = xt_sub[clim][:,62].values
-        if 'LHF' in variables: x_test[clim]['LHF'] = xt_sub[clim][:,63].values
-        if 'LHFns' in variables: x_test[clim]['LHFns'] = xLHFnst_sub[clim].values
-        y_test[clim]['dq_dt'] = yt_sub[clim][:,:30].values
-        y_test[clim]['dT_dt'] = yt_sub[clim][:,30:60].values
+    if xt is not None:
+        for iclim,clim in enumerate(climates):
+            print(clim)
+            x_test[clim] = {}; y_test[clim] = {};
+            if 'p' in variables: x_test[clim]['p'] = P0*hyam+np.outer(xt_sub[clim][:,60].values,hybm)
+            if 'q' in variables: x_test[clim]['q'] = xt_sub[clim][:,:30].values
+            if 'dq_dp' in variables: x_test[clim]['dq_dp'] = p_der_spline(x_test[clim]['q'],x_test[clim]['p'],x_test[clim]['p'],1)
+            if 'dq_dp_FD' in variables: x_test[clim]['dq_dp_FD'] = p_derivative(x_test[clim]['q'],x_test[clim]['p'])
+            if 'd2q_dp2' in variables: x_test[clim]['d2q_dp2'] = p_der_spline(x_test[clim]['q'],x_test[clim]['p'],x_test[clim]['p'],2)
+            if 'd2q_dp2_FD' in variables: x_test[clim]['d2q_dp2_FD'] = p_derivative(x_test[clim]['dq_dp_FD'],x_test[clim]['p'])
+            if 'Q_above' in variables: x_test[clim]['Q_above'] = p_integral(x_test[clim]['q'],x_test[clim]['p'],'above')
+            if 'Q_below' in variables: x_test[clim]['Q_below'] = p_integral(x_test[clim]['q'],x_test[clim]['p'],'below')
+            if 'T' in variables: x_test[clim]['T'] = xt_sub[clim][:,30:60].values
+            if 'dT_dp' in variables: x_test[clim]['dT_dp'] = p_der_spline(x_test[clim]['T'],x_test[clim]['p'],x_test[clim]['p'],1)
+            if 'dT_dp_FD' in variables: x_test[clim]['dT_dp_FD'] = p_derivative(x_test[clim]['T'],x_test[clim]['p'])
+            if 'd2T_dp2' in variables: x_test[clim]['d2T_dp2'] = p_der_spline(x_test[clim]['T'],x_test[clim]['p'],x_test[clim]['p'],2)
+            if 'd2T_dp2_FD' in variables: x_test[clim]['d2T_dp2_FD'] = p_derivative(x_test[clim]['dT_dp_FD'],x_test[clim]['p'])
+            if 'T_above' in variables: x_test[clim]['T_above'] = p_integral(x_test[clim]['T'],x_test[clim]['p'],'above')
+            if 'T_below' in variables: x_test[clim]['T_below'] = p_integral(x_test[clim]['T'],x_test[clim]['p'],'below')
+            if 'RH' in variables: x_test[clim]['RH'] = xRHt_sub[clim].values
+            if 'dRH_dp' in variables: x_test[clim]['dRH_dp'] = p_der_spline(x_test[clim]['RH'],x_test[clim]['p'],x_test[clim]['p'],1)
+            if 'dRH_dp_FD' in variables: x_test[clim]['dRH_dp_FD'] = p_derivative(x_test[clim]['RH'],x_test[clim]['p'])
+            if 'd2RH_dp2' in variables: x_test[clim]['d2RH_dp2'] = p_der_spline(x_test[clim]['RH'],x_test[clim]['p'],x_test[clim]['p'],2)
+            if 'd2RH_dp2_FD' in variables: x_test[clim]['d2RH_dp2_FD'] = p_derivative(x_test[clim]['dRH_dp_FD'],x_test[clim]['p'])
+            if 'RH_above' in variables: x_test[clim]['RH_above'] = p_integral(x_test[clim]['RH'],x_test[clim]['p'],'above')
+            if 'RH_below' in variables: x_test[clim]['RH_below'] = p_integral(x_test[clim]['RH'],x_test[clim]['p'],'below')
+            if 'B' in variables: x_test[clim]['B'] = xBt_sub[clim].values
+            if 'dB_dp' in variables: x_test[clim]['dB_dp'] = p_der_spline(x_test[clim]['B'],x_test[clim]['p'],x_test[clim]['p'],1)
+            if 'dB_dp_FD' in variables: x_test[clim]['dB_dp_FD'] = p_derivative(x_test[clim]['B'],x_test[clim]['p'])
+            if 'd2B_dp2' in variables: x_test[clim]['d2B_dp2'] = p_der_spline(x_test[clim]['B'],x_test[clim]['p'],x_test[clim]['p'],2)
+            if 'd2B_dp2_FD' in variables: x_test[clim]['d2B_dp2_FD'] = p_derivative(x_test[clim]['dB_dp_FD'],x_test[clim]['p'])
+            if 'B_above' in variables: x_test[clim]['B_above'] = p_integral(x_test[clim]['B'],x_test[clim]['p'],'above')
+            if 'B_below' in variables: x_test[clim]['B_below'] = p_integral(x_test[clim]['B'],x_test[clim]['p'],'below')
+            if 'ps' in variables: x_test[clim]['ps'] = xt_sub[clim][:,60].values
+            if 'S0' in variables: x_test[clim]['S0'] = xt_sub[clim][:,61].values
+            if 'SHF' in variables: x_test[clim]['SHF'] = xt_sub[clim][:,62].values
+            if 'LHF' in variables: x_test[clim]['LHF'] = xt_sub[clim][:,63].values
+            if 'LHFns' in variables: x_test[clim]['LHFns'] = xLHFnst_sub[clim].values
+            y_test[clim]['dq_dt'] = yt_sub[clim][:,:30].values
+            y_test[clim]['dT_dt'] = yt_sub[clim][:,30:60].values
         
     return x_train,x_test,y_train,y_test
 
-def range_normalizer(x_train,x_test,scalar_keys,vector_keys,climates=['m4K','ref','p4K'],Norm=None):
+def range_normalizer(x_train,scalar_keys,vector_keys,x_test=None,Norm=None):
     """
     (Optionally) calculates a normalization structure (Norm)
     and normalizes input variables using their range
     x_train = Training set, used to calculate normalization structure
-    x_test = Test set
+    (Optional) x_test = Test set
     scalar_keys = Keys corresponding to scalar variables
     vector_keys = Keys corresponding to vector variables
     """
@@ -208,21 +222,27 @@ def range_normalizer(x_train,x_test,scalar_keys,vector_keys,climates=['m4K','ref
     x_test_range = {}
     for key in scalar_keys:
         x_train[key] = np.outer(x_train[key],np.ones((30,)))
-    for iclim,clim in enumerate(climates):
-        for key in scalar_keys:
-            x_test[clim][key] = np.outer(x_test[clim][key],np.ones((30,)))
+    
+    if x_test is not None: 
+        climates = x_test.keys();
+        for iclim,clim in enumerate(climates):
+            for key in scalar_keys:
+                x_test[clim][key] = np.outer(x_test[clim][key],np.ones((30,)))
+                
     for key in combin_keys: x_train_range[key] = \
         (x_train[key].flatten()-Norm['min'][key])/(Norm['max'][key]-Norm['min'][key])
-    for iclim,clim in enumerate(climates):
-        x_test_range[clim] = {}
-        for key in combin_keys:
-            x_test_range[clim][key] = \
-            (x_test[clim][key].flatten()-\
-             Norm['min'][key])/(Norm['max'][key]-Norm['min'][key])
+    
+    if x_test is not None:
+        for iclim,clim in enumerate(climates):
+            x_test_range[clim] = {}
+            for key in combin_keys:
+                x_test_range[clim][key] = \
+                (x_test[clim][key].flatten()-\
+                 Norm['min'][key])/(Norm['max'][key]-Norm['min'][key])
             
     return x_train_range,x_test_range,Norm
 
-def dic_to_array(key_array,x_train_range,x_test_range,y_train,y_test,scale_dict,climates=['m4K','ref','p4K'],):
+def dic_to_array(key_array,x_train_range,y_train,scale_dict,x_test_range=None,y_test=None):
     """
     Converts dictionary (x_train_range,x_test_range) containing 
     normalized variables into array that can be used for regression
@@ -232,26 +252,30 @@ def dic_to_array(key_array,x_train_range,x_test_range,y_train,y_test,scale_dict,
     # Initialization
     X_train = np.zeros((x_train_range['p'].shape[0],len(key_array)))
     X_test = {};
-    for iclim,clim in enumerate(climates):
-        X_test[clim] = np.zeros((x_test_range[clim]['p'].shape[0],len(key_array)))
+    if x_test_range is not None:
+        climates = x_test_range.keys()
+        for iclim,clim in enumerate(climates):
+            X_test[clim] = np.zeros((x_test_range[clim]['p'].shape[0],len(key_array)))
     
     # Input Assignments
     for ikey,key in enumerate(key_array):
         X_train[:,ikey] = x_train_range[key]
-        for iclim,clim in enumerate(climates):
-            X_test[clim][:,ikey] = x_test_range[clim][key]
+        if x_test_range is not None:
+            for iclim,clim in enumerate(climates):
+                X_test[clim][:,ikey] = x_test_range[clim][key]
     
     # Output Assignments
     dTdt_train = (y_train['dT_dt'] * scale_dict['TPHYSTND']).flatten()
     dQdt_train = (y_train['dq_dt'] * scale_dict['PHQ']).flatten()
     dTdt_test = {}; dQdt_test = {};
-    for iclim,clim in enumerate(climates):
-        dTdt_test[clim] = (y_test[clim]['dT_dt'] * scale_dict['TPHYSTND']).flatten()
-        dQdt_test[clim] = (y_test[clim]['dq_dt'] * scale_dict['PHQ']).flatten()
+    if x_test_range is not None:
+        for iclim,clim in enumerate(climates):
+            dTdt_test[clim] = (y_test[clim]['dT_dt'] * scale_dict['TPHYSTND']).flatten()
+            dQdt_test[clim] = (y_test[clim]['dq_dt'] * scale_dict['PHQ']).flatten()
         
     return X_train,X_test,dQdt_train,dQdt_test,dTdt_train,dTdt_test
 
-def SFS_poly(features,X_train,X_test,Y_train,Y_test,min_features,max_features,cv,climates=['m4K','ref','p4K'],):
+def SFS_poly(features,X_train,Y_train,min_features,max_features,cv,X_test=None,Y_test=None):
     """
     Sequential feature selection based on polynomial fits
     features = Inputs to select
@@ -276,9 +300,11 @@ def SFS_poly(features,X_train,X_test,Y_train,Y_test,min_features,max_features,cv
         ypred = lin_reg.predict(X_transformed)
         mse_train = mean_squared_error(Y_train, ypred)
         mse_test = {}; ypred_test = {};
-        for iclim,clim in enumerate(climates):
-            ypred_test[clim] = lin_reg.predict(sfs.transform(X_test[clim]))
-            mse_test[clim] = mean_squared_error(Y_test[clim], ypred_test[clim])
+        if X_test is not None:
+            climates = X_test.keys()
+            for iclim,clim in enumerate(climates):
+                ypred_test[clim] = lin_reg.predict(sfs.transform(X_test[clim]))
+                mse_test[clim] = mean_squared_error(Y_test[clim], ypred_test[clim])
 
         # Output dictionary
         dict_exp = {}
@@ -286,7 +312,7 @@ def SFS_poly(features,X_train,X_test,Y_train,Y_test,min_features,max_features,cv
             dict_exp[selected_features[i]] = lin_reg.coef_[i]
         dict_exp['LR_Bias'] = lin_reg.intercept_
         dict_exp['mse_train'] = mse_train
-        dict_exp['mse_test'] = mse_test
+        if X_test is not None: dict_exp['mse_test'] = mse_test
         print(dict_exp,'\n')
         dict_output['Number of variables %d'%no_features] = dict_exp
     
